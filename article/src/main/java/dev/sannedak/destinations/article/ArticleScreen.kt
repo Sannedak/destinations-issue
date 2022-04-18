1c559c066c2ed636fdef59bf95705fc6a525e17b
Max Turchin <maxsim.turchin@gmail.com>
2022-04-18 22:54:52 +0600

    Initial commit

---
+++ article/src/main/java/dev/sannedak/destinations/article/ArticleScreen.kt
@@ -0,0 +1,11 @@
+package dev.sannedak.destinations.article
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+import dev.sannedak.destinations.navigation.ArticleDetailsNavArgs
+
+@Destination(navArgsDelegate = ArticleDetailsNavArgs::class)
+@Composable
+fun ArticleDetailsScreen() {
+
+}
---
+++ navigation/src/main/java/dev/sannedak/destinations/navigation/ArticleDetailsNavArgs.kt
@@ -0,0 +1,3 @@
+package dev.sannedak.destinations.navigation
+
+data class ArticleDetailsNavArgs(val id: Int)
